e89a2b2349dd5e3f0f1db4fd8e11842fe72c694f
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-11-12 06:15:59 +0700

    Update assignment 3

--- ass3.cpp
+++ ass3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <map>
 using namespace std;
 
 int main(){
@@ -10,28 +10,27 @@ int main(){
     int l = alphabet.length();
     cin >> n;
     vector<uint32_t> v(l);
-    uint32_t c = 1;
+    map<int,char> m;
+    uint32_t c = 2;
+    m[1] = 'A';
+    v[0] = 1;
     for (int i = 1;i<=l;i++){
-        v[i-1] = c*2 - 1;
+        v[i] = c;
+        m[c] = alphabet[i]; 
         c*=2;
     }
     int idx = l-1;
-    // for (int i = 0;i<l;i++){
-    //     cout<<v[i]<<endl;
-    // }
-    n--;
-    while (v[idx] != n){
+    while (true){
         for (int i = idx;i>=0;i--){
             if (v[i] < n){
+                n-= v[i];
                 idx = i;
                 break;
             }else if (v[i] == n){
-                cout << alphabet[i] <<endl;
+                cout << m[n];
                 return 0;
             }
         }
-        n-= v[idx];
     }
-    cout << alphabet[idx] <<endl;
     return 0;
 }   
